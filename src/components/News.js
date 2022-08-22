cb69db3abf55a4bf8ca287323e065ac87ca86749
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-22 11:55:13 +0500

    Add structure of news item

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is news component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className='container my-3'>
+        <h2>Newsapp - Top headlines</h2>
+        <div className='row'>
+          <div className='col-md-4'>
+            <NewsItem title="my title" description="my description"/>
+          </div>
+          <div className='col-md-4'>
+            <NewsItem title="my title" description="my description"/>
+          </div>
+          <div className='col-md-4'>
+            <NewsItem title="my title" description="my description"/>
+          </div>
+        </div>
       </div>
     )
   }
