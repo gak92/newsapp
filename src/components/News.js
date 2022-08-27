01ef6943126c2b03dae53b518d85b1f94170bafd
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-27 19:22:02 +0500

    Add functionality to load next and previous pages

--- src/components/News.js
+++ src/components/News.js
@@ -2,22 +2,52 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  baseURL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fa4b6d2d25754f26930b6b46e97af535';
   constructor() {
     super();
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1,
     }
   }
 
   async componentDidMount() {
-    let   URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fa4b6d2d25754f26930b6b46e97af535';
-    let data = await fetch(URL);
+    let requestedURL = `${this.baseURL}&page=1&pageSize=20`;
+    let data = await fetch(requestedURL);
+    let parseData = await data.json();
+    console.log('Requested URL: ', requestedURL);
+    this.setState({
+      articles: parseData.articles,
+      totalResults: parseData.totalResults,
+    })
+  }
+
+  handlepreviousClick = async () => {
+    let requestedURL = `${this.baseURL}&page=${this.state.page-1}&pageSize=20`;
+    let data = await fetch(requestedURL);
     let parseData = await data.json();
     console.log(parseData);
     this.setState({
       articles: parseData.articles,
-    })
+      page: this.state.page-1,
+    });
+  }
+
+  handlenextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)) {
+
+    }
+    else {
+      let requestedURL = `${this.baseURL}&page=${this.state.page+1}&pageSize=20`;
+      let data = await fetch(requestedURL);
+      let parseData = await data.json();
+      console.log(parseData);
+      this.setState({
+        articles: parseData.articles,
+        page: this.state.page+1,
+      });
+    }
   }
 
   render() {
@@ -37,6 +67,24 @@ export class News extends Component {
           </div>
           )
           })}
+        </div>
+        <div className='d-flex justify-content-between'>
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={this.handlepreviousClick}
+            disabled={this.state.page<=1}
+            >
+              &larr; Previous
+          </button>
+          <button 
+            type="button" 
+            className="btn btn-info"
+            onClick={this.handlenextClick}
+            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/20)}
+            >
+              Next &rarr; 
+          </button>
         </div>
       </div>
     )
